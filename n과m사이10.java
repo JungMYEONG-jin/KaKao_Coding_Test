73ddb878ed8c1566a9319064623fff94c5e2bb0b
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2022-02-22 17:03:06 +0900

    backtracking

---
+++ n과m사이10.java
@@ -0,0 +1,55 @@
+package hello.login;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int n, m;
+    public static int[] arr = new int[9];
+    public static int[] res;
+    public static boolean[] visited = new boolean[9];
+    public static StringBuilder sb = new StringBuilder();
+
+
+    public static void dfs(int idx, int cnt)
+    {
+        if(m==cnt)
+        {
+            for(int i=0;i<m;i++)
+            {
+                sb.append(arr[i]+" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        int prev = -1;
+        for(int i=idx;i<n;i++)
+        {
+                if(!visited[i] && (prev==0 | prev!=res[i])) {
+
+                    arr[cnt] = res[i];
+                    prev = res[i];
+                    visited[i] = true;
+                    dfs(i,cnt + 1);
+                    visited[i] = false;
+
+                }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        res = new int[n];
+        for(int i=0;i<n;i++)
+            res[i] = sc.nextInt();
+        Arrays.sort(res);
+        dfs( 0,0);
+        System.out.println(sb);
+    }
+
+}
