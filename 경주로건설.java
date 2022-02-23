68849dced8b66106afa80303df668fa53b1579c9
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2022-02-23 22:35:55 +0900

    Update 경주로건설.java

--- 경주로건설.java
+++ 경주로건설.java
@@ -1,118 +1,88 @@
-package hello.login;
-
 import java.util.*;
 
-public class Main {
-
-    public static int n;
-
-    public static int[] board = new int[15];
-    public static int[][] map = new int[26][26];
-    public static boolean[][] visited = new boolean[26][26];
-
-    public static int[] dx = {1, -1, 0, 0};
-    public static int[] dy = {0, 0, 1, -1};
-    public static int limit;
-
-    public static boolean is_prime(int n)
+class Solution {
+    
+    public static int[][] map;
+    public static boolean[][][] visited;
+    public static int total = Integer.MAX_VALUE;
+    public static int len;
+    
+    public static int[] dx = {-1,0, 1, 0};
+    public static int[] dy = {0, -1, 0, 1};
+    
+    
+    public static void bfs(int x, int y, int direction, int cost)
     {
-        if(n<=1)
-            return false;
-        for(int i=2;i<=Math.sqrt(n);i++)
-        {
-            if(n%i==0)
-                return false;
-        }
-        return true;
-    }
-
-   
-
-    public static int solution(int[][] board) {
-        int answer = 0;
-
-        int len = board.length;
-        for(int i=0;i<len;i++)
-        {
-            for(int j=0;j<len;j++)
-            {
-                map[i][j] = board[i][j];
-            }
-        }
-
+        
         Queue<Path> q = new LinkedList<>();
-        q.add(new Path(0, 0, 0)); //0,0 시작
+        q.add(new Path(x, y, direction, cost));
+        visited[x][y][0] = visited[x][y][1] =visited[x][y][2] =visited[x][y][3] = true;
         while(!q.isEmpty())
         {
             Path it = q.poll();
-            int x = it.getX();
-            int y = it.getY();
-            int cost = it.getCost();
+            int px = it.x;
+            int py = it.y;
+            int pDirection = it.direction;
+            int pCost = it.cost;
+            
+            if(px==len-1 && py==len-1)
+            {
+                total = Math.min(total, pCost);
+            }
             for(int i=0;i<4;i++)
             {
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if(nx>=0 && nx<limit && ny>=0 && ny<limit)
+                int nx = px+dx[i];
+                int ny = py+dy[i];
+                int nDirection = i;
+                int nCost = pCost;
+                
+                if(nx<0 || nx>=len || ny<0 || ny>=len ||map[nx][ny]==1)
+                    continue;
+                
+                if(pDirection==-1)
+                    nCost+=100;
+                else if(pDirection==nDirection)
+                    nCost+=100;
+                else
+                    nCost+=600; // 수직에 코너
+                    
+            
+                if(!visited[nx][ny][nDirection] || map[nx][ny]>=nCost)
                 {
-                    if(!visited[nx][ny])
-                    {
-                        visited[nx][ny] = true;
-                        //코너?
-                        
-                        //직선도로
-                    }
+                        visited[nx][ny][nDirection] = true;
+                        map[nx][ny] = nCost;
+                        q.add(new Path(nx, ny, nDirection, nCost));
                 }
+                
             }
         }
-
-
-
+        
+    }
+    public int solution(int[][] board) {
+        int answer = 0;
+        len = board.length;
+        map = board;
+        visited = new boolean[len][len][4];
+        bfs(0, 0, -1, 0);
+        answer = total;
         return answer;
     }
-
+    
     static class Path
     {
-        private int x;
-        private int y;
-        private int cost;
-
-        public Path(int x, int y, int cost) {
+        // 귀찮으니 public으로 하자..
+        public int x;
+        public int y;
+        public int direction;
+        public int cost;
+        
+        public Path(int x, int y, int direction, int cost)
+        {
             this.x = x;
             this.y = y;
+            this.direction = direction;
             this.cost = cost;
         }
-
-        public int getX() {
-            return x;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-
-        public int getCost() {
-            return cost;
-        }
-
-        public void setCost(int cost) {
-            this.cost = cost;
-        }
+     
     }
-
-
-
-
-
-    public static void main(String[] args) {
-
-    }
-
 }
