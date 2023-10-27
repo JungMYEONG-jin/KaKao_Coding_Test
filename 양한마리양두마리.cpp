863bc8c3649d4e4c44489ff330076b541574876c
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2023-10-27 16:10:20 +0900

    Create 양한마리양두마리.cpp
    BFS

---
+++ 양한마리양두마리.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+#include <random>
+#include <numeric>
+#include <cstring>
+
+#define INF 999999999
+
+using namespace std;
+
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int bfs(vector<string>& arr, int n, int m){
+    bool visited[n][m];
+    memset(visited, false, sizeof(visited));
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        for (size_t j = 0; j < m; j++)
+        {
+            if (arr[i][j]=='#' && !visited[i][j])
+            {
+                /* code */
+                visited[i][j] = true;
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                while(!q.empty()){
+                    auto it = q.front();
+                    q.pop();
+                    int x = it.first;
+                    int y = it.second;
+
+                    for(int d=0;d<4;d++){
+                        int nx = x+dx[d];
+                        int ny = y+dy[d];
+
+                        if (nx<0 || nx>=n || ny<0 || ny>=m)
+                        {
+                            continue;
+                        }
+
+                        if (visited[nx][ny] || arr[nx][ny]!='#')
+                        {
+                            continue;
+                        }
+                        
+                        visited[nx][ny] = true;
+                        q.push({nx, ny});
+                    }
+                }
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    // input
+    ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        string s;
+        vector<string> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+
+        cout<<bfs(arr, n, m)<<endl;
+    }
+
+    return 0;
+}
